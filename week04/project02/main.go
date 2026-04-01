e68453da318dca6a6bd2e8612c93468d7fbb28cd
hx2068628-afk <hx2068628@gmail.com>
2026-04-01 10:35:59 +0800

    第四周第二天

---
+++ week04/project02/main.go
@@ -0,0 +1,44 @@
+package main 
+import (
+	"fmt"
+)
+
+func send(intChan chan int){
+	for i:=1;i<=3;i++{
+		intChan<-i
+	}
+	close(intChan)
+
+}
+func receive(intChan chan int,overChan chan bool){
+	for v:=range intChan{
+		fmt.Println(v)
+	}
+	for{
+		v,ok:=<-intChan
+		if !ok{
+			fmt.Println(ok)
+			break
+		}
+			fmt.Println(v)
+
+	}
+	overChan<-true
+	close(overChan)
+}
+
+
+func main(){
+	intChan :=make(chan int,3)
+	overChan :=make(chan bool,1)
+	go send(intChan)
+	go receive(intChan,overChan)
+	for{
+		_,ok:=<-overChan
+		if !ok{
+			fmt.Println(ok)
+			break
+		}
+	}
+
+}
