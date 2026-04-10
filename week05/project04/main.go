6e25d4c7a6d696356fccb9c91bab5eba3a86ce46
hx2068628-afk <hx2068628@gmail.com>
2026-04-10 13:29:06 +0800

    第五周第五天

--- week05/project04/main.go
+++ week05/project04/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -20,23 +21,23 @@ func main() {
 	// defer db.Close()
 	// var stu Student
 	//单条查询
-	// var name string
+	var name string
 	// err = db.QueryRow("select name from students where id = ?", 2).Scan(&name)
 	// fmt.Printf("%#v\n", name)
 	//多条查询
-	// rows, _ := db.Query("select name from students ")
-	// defer rows.Close()
-	// for rows.Next() {
-	// 	rows.Scan(&name)
-	// 	fmt.Println(name)
-	// }
+	rows, _ := db.Query("select name from students ")
+	defer rows.Close()
+	for rows.Next() {
+		rows.Scan(&name)
+		fmt.Println(name)
+	}
 	//增加
 	// db.Exec("insert into students (name,age,id) values(?,?,?)", "王五", 19, 4)
 	// db.QueryRow("select name from students where id = ?", 4).Scan(&name)
 	// fmt.Printf("%#v\n", name)
 	//删除
-	db.Exec("delete from students where id=?", 4)
-	//修改
-	db.Exec("update students set name=? where id=?", "王五", 2)
+	// db.Exec("delete from students where id=?", 4)
+	// //修改
+	// db.Exec("update students set name=? where id=?", "王五", 2)
 
 }
---
+++ week05/project05/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type UserInfo struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func main() {
+	r := gin.Default()
+	db, err1 := sql.Open("mysql", "root:123456@(127.0.0.1:3306)/test?charset=utf8mb4")
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+	// createtext := "create table if not exists user(id int,name varchar(10),age int);"
+	// _, err2 := db.Exec(createtext)
+	// if err2 != nil {
+	// 	log.Fatal(err2)
+	// }
+	// inserttext := "insert into user (id,name,age) values (1,'张三',18),(2,'李四',19),(2,'王五',20)"
+	// _, err3 := db.Exec(inserttext)
+	// if err3 != nil {
+	// 	log.Fatal(err3)
+	// }
+	r.GET("/api/user", func(c *gin.Context) {
+
+		Rows, _ := db.Query("select id,name,age from user")
+		var usersilce = make([]UserInfo, 0)
+		for Rows.Next() {
+			var user UserInfo
+			err := Rows.Scan(&user.Id, &user.Name, &user.Age)
+			if err != nil {
+				log.Fatal(err)
+			}
+			usersilce = append(usersilce, user)
+		}
+		defer Rows.Close()
+		c.JSON(http.StatusOK, gin.H{
+			"data": usersilce,
+		})
+	})
+	r.GET("/api/user/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var user UserInfo
+		db.QueryRow("select id,name,age from user where id=?", id).Scan(&user.Id, &user.Name, &user.Age)
+		c.String(http.StatusOK, "查询结果为%v", user)
+	})
+	r.POST("api/user", func(c *gin.Context) {
+		var user UserInfo
+		c.ShouldBind(&user)
+		_, err := db.Exec("insert into user (id,name,age) values(?,?,?)", user.Id, user.Name, user.Age)
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, user)
+	})
+	r.Run()
+
+}
